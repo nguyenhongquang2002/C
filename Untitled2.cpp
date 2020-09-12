f6b59b300ba8d561397af1ff2039e5680f7f9208
nguyenhongquang2002 <71063187+nguyenhongquang2002@users.noreply.github.com>
2020-09-12 08:48:48 +0700

    bai tap ve nha 1

---
+++ Untitled2.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+	
+	int a;
+	printf ("nhap a=");
+	scanf ("%d",&a);
+	
+	int b;
+	printf ("nhap b=");
+	scanf ("%d",&b);
+	int c;
+	printf ("nhap c=");
+	scanf ("%d",&c)
+	
+	int M;
+	if(a=c){
+	   M=a;
+	}eles{
+	   M=b;
+	}
+    if(M=c){
+       M=c;
+   }
+   printf("Max: %d",M);
+}
+
